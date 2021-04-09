78d10a31e62cab08f7e19944c9aa79f8c8a405e6
KevinZonda <33132228+KevinZonda@users.noreply.github.com>
2021-04-09 14:56:02 +0100

    add playground

---
+++ KWeb.Playground/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+
+using KWeb.Server;
+
+namespace KWeb.Playground
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HttpServer hs = new HttpServer("127.0.0.1", 80);
+            hs.OnHttpRequest += Hs_OnHttpRequest;
+            hs.Start();
+            while (true)
+            {
+                // Keep Server Working
+            }
+
+        }
+
+        private static HttpResponse Hs_OnHttpRequest(HttpRequest request)
+        {
+            return new HttpResponse()
+            {
+                ResponseCode = 200,
+                ResponseStream = GenerateStreamFromString(
+                    "Mthod => " + request.Method + "\n" +
+                    "Uri   => " + request.Uri + "\n" +
+                    "Data  => " + request.ReadToEnd()
+                    )
+            };
+        }
+
+        public static Stream GenerateStreamFromString(string s)
+        {
+            var stream = new MemoryStream();
+            var writer = new StreamWriter(stream);
+            writer.Write(s);
+            writer.Flush();
+            stream.Position = 0;
+            return stream;
+        }
+    }
+}
