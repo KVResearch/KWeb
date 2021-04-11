8e5dd6f13ce67a84602492ea78db4df31a812ca0
KevinZonda <33132228+KevinZonda@users.noreply.github.com>
2021-04-11 22:38:01 +0100

    add Message to HttpException

--- KWeb.Server/HttpException.cs
+++ KWeb.Server/HttpException.cs
@@ -6,15 +6,26 @@ namespace KWeb.Server
     {
         public int ResponseCode { get; }
 
-        public HttpException(int code)
+        public string Message { get; }
+
+        public HttpException(int code, string message = null)
         {
             ResponseCode = code;
+            Message = message;
         }
+
+        public string ToHtml()
+        {
+            return
+                $@"<center><h1>{ResponseCode} {ResponseCodes.Codes[ResponseCode]}</h1><hr>{Info.ServerName} {Info.Version}</center><br>{Message}";
+        }
+
 
         public static HttpResponse GetExpResponse(int code, string msg = null)
         {
             return HttpUtil.GenerateHttpResponse(
-                $@"<center><h1>{code} {ResponseCodes.Codes[code]}</h1><hr>{Info.ServerName} {Info.Version}</center><br>{msg}", code, "text/html");
+                $@"<center><h1>{code} {ResponseCodes.Codes[code]}</h1><hr>{Info.ServerName} {Info.Version}</center><br>{msg}",
+                code, "text/html");
         }
     }
 }
